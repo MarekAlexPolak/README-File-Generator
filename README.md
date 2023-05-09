15292a74754602713faf555088d6b6c2842286c4
Marek Polak <markalexpolak@gmail.com>
2023-05-08 20:06:16 -0400

    added code to generateMarkdown.JS

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# README-File-Generator
+
+  (<img src="https://img.shields.io/badge/License-N/A-blue">,)
+  # README File Generator
+
+  ## Description
+
+  A node JS script which created a professional README page.
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+
+  - [Credits](#credits)
+
+  - [License](#license)
+
+
+  ## Installation
+
+  run node.js and install inquirer using npm i inquirer
+
+  ## Usage
+
+  First run the code in node JS using node index.js command. Then simply follow the prompts
+
+  ## Credits
+
+  N/A
+
+  ## License
+    
+    N/A
+
+
+
+  ## How to Contribute
+  
+  N/A
+
+  ## Tests 
+
+  N/A
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license, colour) {
+  if(license === '' || license === null){
+    return '';
+  }
+  else {
+    return `(<img src="https://img.shields.io/badge/License-${license}-${colour}">,)`
+  }
+
+}
 
 // TODO: Create a function that returns the license link
+function renderLicenseLink(license) {
+  if(license === '' || license === null){
+    return '';
+  }
+  else {
+    return `- [License](#license)`
+  }
+}
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license) {
+  if(license === '' || license === null){
+    return '';
+  }
+  else {
+    return `## License
+    
+    ${license}`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+  return `
+  ${renderLicenseBadge(data.license, data.colour)}
+  # ${data.title}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+
+  - [Credits](#credits)
+
+  ${renderLicenseLink(data.license)}\n
+
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ${renderLicenseSection(data.license)}
+
+
+
+  ## How to Contribute
+  
+  ${data.contributing}
+
+  ## Tests 
+
+  ${data.tests}`;
 }
 
 module.exports = generateMarkdown;
